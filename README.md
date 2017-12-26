eeaf54195e9001b05a0db06e347ea177c2e7961e
Sandro Wong <saloiofun@gmail.com>
2017-12-25 17:50:37 -0800

    Update README.md and code

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # GifTastic
-Use the GIPHY API to make a dynamic web page that populates with gifs of your choice
+
+![demo](assets/images/demo.png)
+
+Uses the GIPHY API to make a dynamic web page that populates with gifs of your choice
+
+Link: https://saloiofun.github.io/GifTastic/
+
+## Getting Started
+
+* Type in the search bar to load gifs related to your keywords.
+
+* Click on any gif to start the animation.
+
+* Click on "Link" button to copy the URL.
--- assets/javascript/giftastic.js
+++ assets/javascript/giftastic.js
@@ -1,165 +1,163 @@
-$(document).ready(function() {
+$(document).ready(function () {
+  var gifButtons = ['Luke Skywalker', 'Yoda', 'Boba Fett', 'Chewbacca']
+  var btnsTypes = ['btn-primary', 'btn-secondary', 'btn-success', 'btn-info', 'btn-warning', 'btn-danger']
+  const APIKEY = '3703fe99db3742bf9c7915cad1d54adc'
 
-	var gifButtons = ["Luke Skywalker", "Yoda", "Boba Fett", "Chewbacca"];
-	var btnsTypes = ["btn-primary", "btn-secondary", "btn-success", "btn-info", "btn-warning", "btn-danger"]
-	const APIKEY = "3703fe99db3742bf9c7915cad1d54adc";
+  var renderGifButtons = function () {
+    $('#gif-buttons').empty()
+    for (var i = 0; i < gifButtons.length; i++) {
+      var index = Math.floor((Math.random() * btnsTypes.length))
+      var button = $('<button type="button" class="btn ' + btnsTypes[index] + ' gif-btns">').attr('data-name', gifButtons[i]).text(gifButtons[i])
+      $('#gif-buttons').append(button)
+    }
+  }
 
-	var renderGifButtons = function() {
-		$("#gif-buttons").empty();
-		for (var i = 0; i < gifButtons.length; i++) {
-			var index = Math.floor((Math.random() * btnsTypes.length));
-			var button = $("<button type=\"button\" class=\"btn " + btnsTypes[index] + " gif-btns\">").attr("data-name", gifButtons[i]).text(gifButtons[i]);
-			$("#gif-buttons").append(button);
-		}
-	}
+  var toggleAnimation = function () {
+    var status = $(this).attr('data-status')
+    if (status === 'still') {
+      $(this).attr('src', $(this).attr('data-animate'))
+      $(this).attr('data-status', 'animate')
+    } else {
+      $(this).attr('src', $(this).attr('data-still'))
+      $(this).attr('data-status', 'still')
+    }
+  }
 
-	var toggleAnimation = function() {
-		var status = $(this).attr("data-status");
-		if (status === "still") { 
-			$(this).attr("src", $(this).attr("data-animate")) 
-			$(this).attr("data-status", "animate");
-		} else {
-			$(this).attr("src", $(this).attr("data-still")) 
-			$(this).attr("data-status", "still");
-		}
-	}
+  var ajaxCall = function (queryURL) {
+    $.ajax({
+      url: queryURL,
+      method: 'GET'
+    }).done(function (response) {
+      var result = response.data
+      var $grid = $('.grid').masonry({
+        itemSelector: '.grid-item',
+        columnWidth: 200,
+        gutter: 10,
+        fitWidth: true
+      })
+      for (var i = 0; i < result.length; i++) {
+        var gifItem = $('<div class="grid-item text-right">')
+        var gif = $('<img class="gif">').attr('src', result[i].images.fixed_width_still.url).attr('data-status', 'still')
+        gif.attr('data-still', result[i].images.fixed_width_still.url)
+        gif.attr('data-animate', result[i].images.fixed_width.url)
+        var gifUrl = $('<span class="badge badge-info gif-url" data-clipboard-text=" ' + result[i].images.fixed_width.url + '"><i class="fa fa-link" aria-hidden="true"></i> Link</span>')
+        var rating = $('<span class="badge badge-default">Rating: ' + '<span class="text-uppercase">' + result[i].rating + '</span></span>')
+        gifItem.append(gif)
+        gifItem.append(gifUrl)
+        gifItem.append(rating)
+        $('#gif-container').append(gifItem).masonry('appended', gifItem)
+      }
+      $grid.imagesLoaded().progress(function () {
+        $grid.masonry('layout')
+      })
+    })
+  }
 
-	var ajaxCall = function(queryURL) {
-		$.ajax({
-			url: queryURL,
-			method: "GET"
-		}).done(function(response) {
-			var result = response.data;
-			var $grid = $('.grid').masonry({
-				itemSelector: '.grid-item',
-				columnWidth: 200,
-				gutter: 10,
-				fitWidth: true
-			});
-			for (var i = 0; i < result.length; i++) {
-				var gifItem = $("<div class=\"grid-item text-right\">");
-				var gif = $("<img class=\"gif\">").attr("src", result[i].images.fixed_width_still.url).attr("data-status", "still");
-				gif.attr("data-still", result[i].images.fixed_width_still.url);
-				gif.attr("data-animate", result[i].images.fixed_width.url);
-				var gifUrl = $("<span class=\"badge badge-info gif-url\" data-clipboard-text=\" " + result[i].images.fixed_width.url + "\"><i class=\"fa fa-link\" aria-hidden=\"true\"></i> Link</span>");
-				var rating = $("<span class=\"badge badge-default\">Rating: " + "<span class=\"text-uppercase\">" + result[i].rating + "</span></span>");
-				gifItem.append(gif);
-				gifItem.append(gifUrl);
-				gifItem.append(rating);
-				$("#gif-container").append(gifItem).masonry( 'appended', gifItem );;
-			}
-			$grid.imagesLoaded().progress( function() {
-				$grid.masonry('layout');
-			});
-		});
-	}
+  var renderSearchGifs = function (input) {
+    $('#gif-container').empty()
+    var queryURL = 'https://api.giphy.com/v1/gifs/'
+    queryURL += 'search?' + $.param({
+      'api_key': APIKEY,
+      'q': input
+    })
+    ajaxCall(queryURL)
+  }
 
-	var renderSearchGifs = function(input) {
-		$("#gif-container").empty();
-		var queryURL = "https://api.giphy.com/v1/gifs/";
-		queryURL += 'search?' + $.param({
-			'api_key': APIKEY,
-			'q' : input
-		});
-		ajaxCall(queryURL);
-	}
+  var renderTrending = function () {
+    $('#page-header').html('<h1 class="h-title">Trending Now</h1>')
+    $('#gif-container').empty()
+    var queryURL = 'https://api.giphy.com/v1/gifs/'
+    queryURL += 'trending?' + $.param({
+      'api_key': APIKEY
+    })
+    ajaxCall(queryURL)
+  }
 
-	var renderTrending = function() {
-		$("#page-header").html("<h1 class=\"h-title\">Trending Now</h1>");
-		$("#gif-container").empty();
-		var queryURL = "https://api.giphy.com/v1/gifs/";
-		queryURL += 'trending?' + $.param({
-			'api_key': APIKEY
-		});
-		ajaxCall(queryURL);
-	}
+  $('#search-submit').on('click', function (event) {
+    event.preventDefault()
+    var input = $('#search-input').val().trim()
+    if (input) {
+      gifButtons.push(input)
+      renderGifButtons()
+      renderSearchGifs(input)
+    } else {
+      renderTrending()
+    }
+  })
 
-	$("#search-submit").on("click", function(event) {
-		event.preventDefault();
-		input = $("#search-input").val().trim();
-		if (input) {
-			gifButtons.push(input);
-			renderGifButtons();
-			renderSearchGifs(input);
-		} else {
-			renderTrending();
-		}
-	});	
+  $('#play').on('click', function () {
+    $('.gif').each(function () {
+      $(this).attr('src', $(this).attr('data-animate'))
+      $(this).attr('data-status', 'animate')
+    })
+  })
 
-	$("#play").on("click", function() {
-		$(".gif").each(function () {
-			$(this).attr("src", $(this).attr("data-animate")) 
-			$(this).attr("data-status", "animate");
-		});
-	});
+  $('#stop').on('click', function () {
+    $('.gif').each(function () {
+      $(this).attr('src', $(this).attr('data-still'))
+      $(this).attr('data-status', 'still')
+    })
+  })
 
-	$("#stop").on("click", function() {
-		$(".gif").each(function () {
-			$(this).attr("src", $(this).attr("data-still")) 
-			$(this).attr("data-status", "still");
-		});
-	});
+  $(document).on('click', '.gif', toggleAnimation)
 
-	$(document).on("click", ".gif", toggleAnimation);
+  $(document).on('click', '#trending', renderTrending)
 
-	$(document).on("click", "#trending", renderTrending);
+  $(document).on('click', '#reactions', function () {
+    $('#page-header').html('<h1 class="h-title">Reactions</h1>')
+    renderSearchGifs('Reactions')
+  })
 
-	$(document).on("click", "#reactions", function() {
-		$("#page-header").html("<h1 class=\"h-title\">Reactions</h1>");
-		renderSearchGifs("Reactions");
-	});
+  $(document).on('click', '#entertainment', function () {
+    $('#page-header').html('<h1 class="h-title">Entertainment</h1>')
+    renderSearchGifs('Entertainments gifs')
+  })
 
-	$(document).on("click", "#entertainment", function() {
-		$("#page-header").html("<h1 class=\"h-title\">Entertainment</h1>");
-		renderSearchGifs("Entertainments gifs");
-	});
+  $(document).on('click', '#memes', function () {
+    $('#page-header').html('<h1 class="h-title">Memes</h1>')
+    renderSearchGifs('Memes')
+  })
 
-	$(document).on("click", "#memes", function() {
-		$("#page-header").html("<h1 class=\"h-title\">Memes</h1>");
-		renderSearchGifs("Memes");
-	});
+  $(document).on('click', '.gif-btns', function () {
+    var input = $(this).attr('data-name')
+    $('#page-header').html('<h1 class="h-title text-capitalize">' + input + '</h1>')
+    renderSearchGifs(input)
+  })
 
-	$(document).on("click", ".gif-btns", function() {
-		var input = $(this).attr("data-name");
-		$("#page-header").html("<h1 class=\"h-title text-capitalize\">" + input + "</h1>");
-		renderSearchGifs(input);
-	});
+  $(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+  })
 
-	$(function () {
-		$('[data-toggle="tooltip"]').tooltip()
-	})
+  renderGifButtons()
 
-	renderGifButtons();
+  renderTrending()
 
-	renderTrending();
+  // Tooltip
 
-	// Tooltip
+  $('.gif-url').tooltip({
+    trigger: 'click',
+    placement: 'left'
+  })
 
-	$('.gif-url').tooltip({
-		trigger: 'click',
-		placement: 'left'
-	});
+  function setTooltip (btn, message) {
+    btn.tooltip('hide')
+    .attr('data-original-title', message)
+    .tooltip('show')
+  }
 
-	function setTooltip(btn, message) {
-		btn.tooltip('hide')
-		.attr('data-original-title', message)
-		.tooltip('show');
-	}
+  function hideTooltip (btn) {
+    setTimeout(function () {
+      btn.tooltip('hide')
+    }, 1000)
+  }
 
-	function hideTooltip(btn) {
-		setTimeout(function() {
-			btn.tooltip('hide');
-		}, 1000);
-	}
+  // Clipboard
 
-	// Clipboard
+  var clipboard = new Clipboard('.gif-url')
 
-	var clipboard = new Clipboard('.gif-url');
-
-	clipboard.on('success', function(e) {
-		var btn = $(e.trigger);
-		setTooltip(btn, 'Copied!');
-		hideTooltip(btn);
-	});
-
-});
+  clipboard.on('success', function (e) {
+    var btn = $(e.trigger)
+    setTooltip(btn, 'Copied!')
+    hideTooltip(btn)
+  })
+})
